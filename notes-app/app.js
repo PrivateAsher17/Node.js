fa358b70a1911bae27fb24c827f895c19a36becc
Ashwani Kumar Maurya <49336482+PrivateAsher17@users.noreply.github.com>
2021-10-13 08:01:18 +0530

    131021 Commit

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,51 @@
-const fs = require('fs')
+// const getNotes = require("./notes.js")
+// const validator = require("validator")
+// const fs = require('fs')
 
-fs.appendFileSync('notes.txt','I am a software Engineer')
+// fs.appendFileSync('notes.txt','I am a software Engineer')
+
+// const name = "Ashwani"
+// const sum=add(4,-2)
+// const op = getNotes()
+// console.log(op);
+// console.log(validator.isEmail("email@gmail.com"))
+
+const chalk = require("chalk")
+// console.log(chalk.green.bold("Success..."));
+const yargs = require("yargs")
+// console.log(process.argv)
+
+yargs.command({
+    command:'add',
+    describe:'Add a new note',
+    builder:{
+        title:{
+            describe:"Note Title",
+            demandOption:true,
+            type:"string"
+        }
+    },
+    handler: function(argv){
+        // console.log("Adding a new note!",argv);
+        console.log("Title : "+argv.title);
+    }
+})
+yargs.command({
+    command:'list',
+    describe:'List all the notes',
+    handler: function(){
+        console.log("Listing your notes...");
+    }
+})
+yargs.command({
+    command:'read',
+    describe:'Reading your notes',
+    handler: function(){
+        console.log("Reading your notes");
+    }
+})
+
+yargs.parse()
+
+// console.log(yargs.argv);
+
